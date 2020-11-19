631a8365c4f63d4e5ec97a2640e210769d0a1d90
iyashjayesh <180303105046@paruluniversity.ac.in>
2020-11-19 19:05:33 +0530

    tinder-backend :rocket

---
+++ server.js
@@ -0,0 +1,48 @@
+import express from "express";
+import mongoose from "mongoose";
+import Cors from "cors";
+import Cards from "./dbCards.js";
+
+// App Config
+const app =express();
+const port = process.env.PORT || 8001;
+const connection_url = `mongodb+srv://admin:K0zmQgxakG9UC2ot@cluster0.pvyei.mongodb.net/tinderdb?retryWrites=true&w=majority`;
+
+// Middlewares
+app.use(express.json());
+app.use(Cors());
+
+// DB Config
+mongoose.connect(connection_url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    UseUnifiedTopology: true,
+});
+
+// API Endpoints
+app.get('/', (req, res) => res.status(200).send('Hello Users!!'));
+
+app.post('/tinder/cards', (req, res) => {
+    const dbCard =req.body;
+
+    Cards.create (dbCard, (err, data) => {
+        if(err){
+            res.status(500).send(err);
+        } else{
+            res.status(201).send(data);
+        }
+    });
+});
+
+app.get('/tinder/cards', (req, res) => {
+    Cards.find((err, data) => {
+        if(err){
+            res.status(500).send(err);
+        } else{
+            res.status(200).send(data);
+        }
+    });
+});
+
+// Listener
+app.listen(port, () => console.log(`listening on localhost: ${port}`));
